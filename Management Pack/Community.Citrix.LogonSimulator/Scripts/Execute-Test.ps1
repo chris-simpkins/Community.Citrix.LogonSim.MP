3270f697628e3863c02aa3cad4f6b9c0af5012ea
Chris Simpkins <Chris.Simpkins@squaredup.com>
2018-11-16 12:56:04 +0000

    Updated to use start process instead of process object

--- Management Pack/Community.Citrix.LogonSimulator/Scripts/Execute-Test.ps1
+++ Management Pack/Community.Citrix.LogonSimulator/Scripts/Execute-Test.ps1
@@ -24,23 +24,17 @@ try {
 	# SCOM log start
 	$api.LogScriptEvent($scriptName, 1000, 0, "Started")
 
-	$scriptProcessStartInfo = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
-	$scriptProcessStartInfo.Arguments = "$ConfigurationPath\Scripts\Test-CitrixApp.ps1 -LogFilePath '$ConfigurationPath\Logs\$TestName' -LogFileName 'sflauncher.log' -SiteURL '$StoreFrontUrl' -UserName '$Domain\$UserName' -Password $Password -ResourceName $ResourceName -TimeoutForSFLoginPage $TimeoutForSFLoginPage -TimeoutForSFResourcesPage $TimeoutForSFResourcesPage -TimeoutForSessionLogin $TimeoutForSessionLogin -TimeoutForOther $TimeoutForOther"
-    $scriptProcessStartInfo.Domain = $Domain
-	$scriptProcessStartInfo.UserName = $UserName
-	$scriptProcessStartInfo.Password = (ConvertTo-SecureString $Password -AsPlainText -Force)
-	$scriptProcessStartInfo.LoadUserProfile = $true
-	$scriptProcessStartInfo.Verb = "runas"
-    $scriptProcessStartInfo.UseShellExecute = $false
-	$process = [System.Diagnostics.Process]::Start($scriptProcessStartInfo)
-	$process.WaitForExit()
-	$returnCode = $process.ExitCode
+	$Password = ConvertTo-SecureString $Password -AsPlainText -Force
+
+	$scriptResult = (Start-Process powershell -verb 'runas' -ArgumentList '-File',"$ConfigurationPath\Scripts\Test-CitrixApp.ps1",'-LogFilePath',"$ConfigurationPath\Logs",'-LogFileName','sflauncher.log','-SiteURL',$StoreFrontUrl,'-UserName',"$Domain\$UserName",'-Password',$Password,'-ResourceName',$ResourceName -Wait -PassThru).ExitCode	
 
 	# Log complete
-	$api.LogScriptEvent($scriptName, 1001, 0, "Finished with output '$returnCode'")
+	$api.LogScriptEvent($scriptName, 1001, 0, "Finished with output: $scriptResult")
+
+	$api.LogScriptEvent($scriptName, 1002, 0, '$ConfigurationPath\Logs\$TestName')
 
 	# Interpret errors as SCOM monitor result
-	if($returnCode -eq 0) { $state = "good" } else { $state = "bad" }
+	if($scriptResult -eq 0) { $state = "good" } else { $state = "bad" }
 
 	$api.LogScriptEvent($scriptName, 1001, 0, "Adding state: $state")
 
